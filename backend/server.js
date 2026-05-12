a27eabf6efe1159db5be76edda035c478b9a20ba
Ishan Rajan <irajan@terpmail.umd.edu>
2026-05-12 19:13:53 -0400

    Was able to finish and fix the first endpoint.

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const supabaseClient = require("@supabase/supabase-js");
-const dotnev = require("dotenv");
+const dotenv = require("dotenv");
 
 const app = express();
 const port = 3000;
-dotnev.config();
+app.listen(port);
+
+dotenv.config({ path: "../.env" });
 
 app.use(express.static("frontend/build"));
 app.use(express.json());
@@ -16,10 +18,23 @@ const NVD_API_KEY = process.env.NVD_API_KEY;
 const supabase = supabaseClient.createClient(supabaseUrl, supabaseKey);
 
 // ENDPOINT 1: To get the watchlist of the user from the database
-app.get("/api/watchlist", async (req, res) => {});
+app.get("/api/watchlist", async (req, res) => {
+  try {
+    const { data, error } = await supabase.from("watchlist").select("*");
+    if (error) {
+      console.log(error);
+      return res.status(400).json({ error: error.message });
+    }
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: "Server Failed" });
+  }
+});
 
 // ENDPOINT 2: Update the watchlist on the database
-app.post("/api/watchlist", async (req, res) => {});
+app.post("/api/watchlist", async (req, res) => {
+  req.body;
+});
 
 // ENDPOINT 3: GET Data from the NVD api
 app.get("/api/vulnerabilities", async (req, res) => {});
