45071ba695962a6d0ae15ff62045eefc6015003d
Ishan Rajan <irajan@terpmail.umd.edu>
2026-05-13 02:45:25 -0400

    Finished ENDPOINT 2 and now working on ENDPOINT 3.

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,7 @@ const NVD_API_KEY = process.env.NVD_API_KEY;
 const supabase = supabaseClient.createClient(supabaseUrl, supabaseKey);
 
 // ENDPOINT 1: To get the watchlist of the user from the database
+// Call this endpoint to get the user's watchlist of all of the technologies they are using.
 app.get("/api/watchlist", async (req, res) => {
   try {
     const { data, error } = await supabase.from("watchlist").select("*");
@@ -32,9 +33,49 @@ app.get("/api/watchlist", async (req, res) => {
 });
 
 // ENDPOINT 2: Update the watchlist on the database
+// Call this endpoint to update the user's watchlist on supabase and the frontend.
 app.post("/api/watchlist", async (req, res) => {
-  req.body;
+  try {
+    const { data, error } = await supabase
+      .from("watchlist")
+      .insert({
+        tech_name: req.body.tech_name,
+        user_id: req.body.user_id,
+      })
+      .select("*");
+    // .insert.select only show the new row and hence made it easier for you Phillp
+    if (error) {
+      console.log(error);
+      return res.status(400).json({ error: error.message });
+    }
+    res.json(data[0]);
+  } catch (error) {
+    console.log("Error:", error);
+    res.status(500).json({ message: "Server Failed" });
+  }
 });
 
 // ENDPOINT 3: GET Data from the NVD api
-app.get("/api/vulnerabilities", async (req, res) => {});
+// Borrows the logic from ENDPOINT 1 and 2
+
+app.get("/api/vulnerabilities", async (req, res) => {
+  try {
+    const { data, error } = await supabase.from("watchlist").select("*");
+    if (error) {
+      console.log(error);
+      return res.status(400).json({ error: error.message });
+    }
+
+    let user_watch_list = [];
+
+    for (const row of data) {
+      const data = await fetch(
+        `https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch=${row}`
+      );
+      const information = await data.json();
+      user_watch_list.push(information);
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Server Failed" });
+  }
+});
